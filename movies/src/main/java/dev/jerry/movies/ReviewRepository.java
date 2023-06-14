a8915be9cf78c9c0dd438a1f9923f0fa102dc21e
JerryT-cell <51903753+JerryT-cell@users.noreply.github.com>
2023-06-14 06:26:01 +0200

    Update ReviewRepository.java

--- movies/src/main/java/dev/jerry/movies/ReviewRepository.java
+++ movies/src/main/java/dev/jerry/movies/ReviewRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 
-@Repository //indicate that the class provides the mechanism for storage, retrieval,search, update
-            //and delete operation on objects
-        //repositories are a way to talk to the database
+@Repository 
+        //repositories 
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
